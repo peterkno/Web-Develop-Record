b37226fe6aa89c7d57acb0d770efcbcff1201d10
Chen Ru <peter55180831@gmail.com>
2019-07-27 20:27:36 +0800

    Block Scoped Variables

--- SS-04/2-Hello Webpack/src/main.js
+++ SS-04/2-Hello Webpack/src/main.js
@@ -6,4 +6,16 @@ window.onload = function() {
     console.log(n);
     m1();
     m2();
+
+    let x;
+    for(let i = 0; i < 10; i ++){
+        x = "foo";
+    }
+    console.log(i); //error
+    let y = 10;
+    if(true){
+        y++; //error
+    }
+
+
 };

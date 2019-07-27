77d1e3b4305ac3a8ec4bd20ee842252b09ecb1c4
Chen Ru <peter55180831@gmail.com>
2019-07-27 21:13:37 +0800

    Arrow Function

--- SS-04/2-Hello Webpack/src/main.js
+++ SS-04/2-Hello Webpack/src/main.js
@@ -1,4 +1,4 @@
-import m1, {n} from "./module-1.js";
+import m1, {n, user} from "./module-1.js";
 import m2 from "./module-2.js";
 // import 'babel-polyfill';
 
@@ -6,16 +6,20 @@ window.onload = function() {
     console.log(n);
     m1();
     m2();
+    user.greet();
+    
+    // user.greet2();
 
-    let x;
-    for(let i = 0; i < 10; i ++){
-        x = "foo";
-    }
-    console.log(i); //error
-    let y = 10;
-    if(true){
-        y++; //error
-    }
+    // // Block Scoped Variables
+    // let x;
+    // for(let i = 0; i < 10; i ++){
+    //     x = "foo";
+    // }
+    // console.log(i); //error
+    // let y = 10;
+    // if(true){
+    //     y++; //error
+    // }
 
 
 };
--- SS-04/2-Hello Webpack/src/module-1.js
+++ SS-04/2-Hello Webpack/src/module-1.js
@@ -3,7 +3,27 @@ import './module-1.css';
 
 export default function() {
     var el = document.querySelector('#module-1');
-    el.textContent = _.join(["Module", "1"], " ");
+
+    let module = "module 1";
+    el.textContent = `Hello ${module}`;
+    // el.textContent = _.join(["Module", "1"], " ");
 }
 
 export var n = 1050;
+
+export var user = {
+    name: 'Jane',
+    friends: ['Alice', 'Peter'],
+    greet: function() {
+        // this will be user
+        this.friends.forEach(f => {
+            console.log('Hi ' + f + ", I'm " + this.name);
+        });
+    },
+    greet2: function() {
+        // this will be windows.onload
+        this.friends.forEach(function(element) {
+            console.log("Hi" + element + ", I'm " + this.name);
+        });
+    } 
+};

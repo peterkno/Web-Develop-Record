8ad65cef2d2e00aaa117b713eefcac6752049b94
Ruru <peter105060016@gapp.nthu.edu.tw>
2019-08-29 16:49:59 +0800

    person pdf update

--- test/test_server/src/model/persons.js
+++ test/test_server/src/model/persons.js
@@ -75,23 +75,15 @@ function create(newPerson) {
         })
         console.log("Finish Create Peronal Info(JSON)");
 
-        let tmp = 123;
-        var html = htmlToPdfMake(`
-        <div>
-            <h1>My title</h1>
-            <p>
-            這個句子是${tmp} with a <strong>bold word</strong>, <em>one in italic</em>,
-            and <u>one with underline</u>. And finally <a href="https://www.somewhere.com">a link</a>.
-            </p>
-        </div>
-        `, window);
+        let htmlStr = _listPersonalInfo(newPerson);
+        var html = htmlToPdfMake(htmlStr, window);
         var loopHtml = htmlToPdfMake(loopStr, window);
         var docDefinition = {
             defaultStyle: {
                 font: 'MSFont'
             },
             content: [
-                loopHtml
+                html
             ],
         };
 
@@ -105,6 +97,33 @@ function create(newPerson) {
     })
 }
 
+function _listPersonalInfo(newPerson) {
+    let htmlStr = '';
+    htmlStr += `<div>`;
+    htmlStr += `<h3>會員資訊</h3>`;
+    htmlStr += `<p>中文姓名:${newPerson.chineseName} 英文姓名:${newPerson.chineseName}</p>`;
+    htmlStr += `<br />`
+    htmlStr += `<p>出生年月日:${newPerson.birthYear}年${newPerson.birthMonth}月${newPerson.birthDay}</p>`;
+    htmlStr += `<p>身分證號:${newPerson.personalID}</p>`;
+    htmlStr += `<br />`;
+    htmlStr += `<p>通訊地址:${newPerson.address}</p>`;
+    htmlStr += `<br />`
+    htmlStr += `<p>通訊電話:${newPerson.phone}</p>`;
+    htmlStr += `</div>`;
+
+    return htmlStr;
+}
+function _listChattel(newPerson) {
+    
+}
+function _listArr(arr, type) {
+    let arrStr = '';
+    for(i = 0; i < arr.length; i++) {
+        arrStr += `<p>${i+1}.${type}:${arr[i].value}元</p>`;
+        arrStr += `<br />`;
+    }
+    return arrStr;
+}
 module.exports = {
     list,
     create
